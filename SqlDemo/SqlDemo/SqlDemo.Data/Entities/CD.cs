c27cb6215540a876991ae7619f42294f59509585
James Bender <James@JamesCBender.com>
2014-07-30 21:43:34 -0400

    added entities

---
+++ SqlDemo/SqlDemo/SqlDemo.Data/Entities/CD.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace SqlDemo.Data.Entities
+{
+    public class CD
+    {
+        public int Id { get; set; }
+        public string Artist { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+
+        public virtual ICollection<Track> Tracks { get; set; }
+    }
+}
---
+++ SqlDemo/SqlDemo/SqlDemo.Data/Entities/DVD.cs
@@ -0,0 +1,13 @@
+﻿namespace SqlDemo.Data.Entities
+{
+    public class DVD
+    {
+        public int Id { get; set; }
+        public int RunningTime { get; set; }
+        public bool IsSpecialEdition { get; set; }
+        public string Synopsis { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+    }
+}
